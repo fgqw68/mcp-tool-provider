71dfcda94325385f3a4d1efe97129c5b6ad775fc
sabir vt <sabir.vt@zebra.com>
2026-03-14 07:00:25 +0530

    fix missing library import

--- main.py
+++ main.py
@@ -14,7 +14,9 @@ if __name__ == "__main__":
     try:
         print(f"--- inside main ---")
         # 1. Get port safely
-        final_port = os.getenv("PORT", 8000)
+        # 1. Convert to INT - This is mandatory for mcp.run
+        raw_port = os.getenv("PORT", "8000")
+        final_port = int(raw_port)
         print(f"--- DEBUG: Attempting to start on port {final_port} ---")
 
         # 2. COMMENT THIS OUT temporarily to test Render connectivity
@@ -25,10 +27,9 @@ if __name__ == "__main__":
         
         # 3. Use the most basic run command possible
         mcp.run(
-            transport="sse", 
-            host="0.0.0.0", 
-            port=final_port
-        )
+                transport="sse", 
+                port=final_port
+            )
 
     except Exception as e:
         print(f"--- CRITICAL ERROR DURING STARTUP ---")
