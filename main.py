ebe22fedc1a8b822f1c2889cfebbe7beb87591ac
sabir vt <sabir.vt@zebra.com>
2026-03-14 07:13:32 +0530

    removed port details so mcp can pick its own

--- main.py
+++ main.py
@@ -13,23 +13,8 @@ mcp = FastMCP("Sentinel-Knowledge-Base")
 if __name__ == "__main__":
     try:
         print(f"--- inside main ---")
-        # 1. Get port safely
-        # 1. Convert to INT - This is mandatory for mcp.run
-        raw_port = os.getenv("PORT", "8000")
-        final_port = int(raw_port)
-        print(f"--- DEBUG: Attempting to start on port {final_port} ---")
-
-        # 2. COMMENT THIS OUT temporarily to test Render connectivity
-        # print("Indexing document...")
-        # index_document() 
-
-        print(f"--- DEBUG: Reached mcp.run() ---")
-        
-        # 3. Use the most basic run command possible
-        mcp.run(
-                transport="sse", 
-                port=final_port
-            )
+   
+        mcp.run(transport='sse')
 
     except Exception as e:
         print(f"--- CRITICAL ERROR DURING STARTUP ---")
